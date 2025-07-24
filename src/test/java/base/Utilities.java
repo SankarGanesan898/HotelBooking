ef7e9d06ca519f75e10853eed06ad8ee16855f77
SankarGanesan898 <shivshan.klu@gmail.com>
2025-07-24 13:35:48 +0530

    Scenario create booking added

--- src/test/java/base/Utilities.java
+++ src/test/java/base/Utilities.java
@@ -1,5 +1,44 @@
 package base;
 
+import java.util.Random;
+import org.json.JSONObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 public class Utilities {
+	
+
+	ObjectMapper mapper;
+	protected BookingRequest bookingRequest = new BookingRequest();;
+	String CONTENT_TYPE;
+	protected Response response;
+	RequestSpecification requestSpecification;
+	JSONObject requestBody;
+	
+	public Utilities() {
+		mapper = new ObjectMapper();
+	}
+	
+	public String createRequestBody() throws JsonProcessingException {
+		String requestbody = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(bookingRequest);
+		return requestbody;
+	}
+	
+	public RequestSpecification requestSetup() {
+
+		RestAssured.baseURI = LoadProperties.getProperty("appURL");
+		CONTENT_TYPE = LoadProperties.getProperty("content.type");
+		return RestAssured.given().contentType(CONTENT_TYPE).accept(CONTENT_TYPE);
+	}	
+
+	public String generateRandomRoomId() {
+
+		final Random random = new Random();
+		return String.valueOf(2000 + random.nextInt(900));
+
+	}
 
 }
--- src/test/java/com/booking/stepdefinitions/BookingOperations.java
+++ src/test/java/com/booking/stepdefinitions/BookingOperations.java
@@ -1,5 +1,56 @@
 package com.booking.stepdefinitions;
 
-public class BookingOperations {
+import static org.junit.Assert.assertEquals;
+import java.util.Map;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import base.BookingDates;
+import base.Utilities;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
+public class BookingOperations extends Utilities{	
+	
+	BookingDates dates;
+	String requestBody;
+	int bookingId;
+	
+	public BookingOperations() {	}		
+	
+	@Given("user hits endpoint {string}")
+	public void user_hits_endpoint(String endpoint) {
+		
+		bookingRequest.setEndPoint(endpoint);
+	}
+	
+	@When("user books the room with the given details")
+	public void user_books_the_room_with_the_given_details(final DataTable dataTable) throws JsonProcessingException{
+		
+		int roomid = Integer.parseInt(generateRandomRoomId());
+		dates = new BookingDates();
+		for (Map<String, String> data : dataTable.asMaps(String.class, String.class)) {
+			bookingRequest.setFirstname(data.get("firstname"));
+			bookingRequest.setLastname(data.get("lastname"));
+			bookingRequest.setEmail(data.get("email"));
+			bookingRequest.setPhone(data.get("phone"));
+			dates.setCheckin(data.get("checkin"));
+			dates.setCheckout(data.get("checkout"));
+			bookingRequest.setBookingdates(dates);
+			bookingRequest.setRoomid(roomid);
+			bookingRequest.setDepositpaid(false);
+		}				
+
+		response = requestSetup().body(createRequestBody())
+				.when()
+				.post("api/booking");
+
+	}
+	
+	@Then("the response status code should be {int}")
+	public void the_response_status_code_should_be(int expectedStatusCode) {
+		
+		assertEquals(expectedStatusCode, response.getStatusCode());
+		System.out.println("response code = "+response.getStatusCode());
+	}
 }
